88df5c7dbf8fad93a3f6dfbfd87089843113986f
DayanaDDimitrova <138893908+DayanaDDimitrova@users.noreply.github.com>
2023-07-25 11:37:10 +0000

    comitting for the firts time

---
+++ instalations/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '756227ed82dbad1d9c63e85fbf1c4f1a0f3e0c40',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '756227ed82dbad1d9c63e85fbf1c4f1a0f3e0c40',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
